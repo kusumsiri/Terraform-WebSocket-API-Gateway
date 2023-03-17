61e3cef5144c308626c2f6ce054d77e38562dbda
kusumsiri <kusumsiri@gmail.com>
2023-03-18 04:41:22 +0530

    Adding stage

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 resource "aws_apigatewayv2_api" "chat" {
-  name = "chat-websocket"
-  protocol_type = "WEBSOCKET"
-  route_selection_expression = "$request.body.action"
-  description = "API for chat application"
+  name                          = "chat-websocket"
+  protocol_type                 = "WEBSOCKET"
+  route_selection_expression    = "$request.body.action"
+  description                   = "API for chat application"
+}
+
+resource "aws_apigatewayv2_stage" "chat" {
+  api_id = aws_apigatewayv2_api.chat.id
+  name   = "dev"
+  desdescription = "Development stage" 
 }
