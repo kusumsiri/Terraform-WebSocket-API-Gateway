7c16cf10d849af3ff1ead453c32b7e8a953cfa33
kusumsiri <kusumsiri@gmail.com>
2023-03-19 11:59:44 +0530

    provider edited

--- providers.tf
+++ providers.tf
@@ -8,5 +8,7 @@ terraform {
 
 provider "aws" {
   region   = "us-east-1"
-  role_arn = "arn:aws:iam::847855290887:role/lambda-apigateway-role-Jan21-2023"
+  assume_role {
+    role_arn     = "arn:aws:iam::847855290887:role/lambda-apigateway-role-Jan21-2023"
+  }
 }
