0c8c238f06bced502a7088d711ea1fc5daa2c6b5
kusumsiri <kusumsiri@gmail.com>
2023-03-19 06:38:28 +0530

    Adding lambda function

--- main.tf
+++ main.tf
@@ -15,4 +15,20 @@ resource "aws_apigatewayv2_stage" "chat" {
 resource "aws_apigatewayv2_route" "chat" {
   api_id = aws_apigatewayv2_api.chat.id
   route_key = "$default"
+
+}
+
+module "lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "my-lambda1"
+  description   = "My awesome lambda function"
+  handler       = "index.handler"
+  runtime       = "nodejs 18.x"
+
+  source_path = "../src"
+
+  tags = {
+    Name = "my-lambda1"
+  }
 }
---
+++ src/index.js
@@ -0,0 +1,4 @@
+exports.handler = async function (event) {
+   
+  return {"event":JSON.parse(event)}
+};
