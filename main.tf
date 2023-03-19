ed1a95f5beb74c207c504f59c147f0ac8fed2574
kusumsiri <kusumsiri@gmail.com>
2023-03-19 11:56:57 +0530

    Provider modified

--- main.tf
+++ main.tf
@@ -37,4 +37,5 @@ module "lambda_function" {
     "arn:aws:lambda:us-east-1:847855290887:layer:node_module_aws-sdk:2"
   ]
   cloudwatch_logs_retention_in_days = 1
+  # event_source_mapping = 
 }
--- providers.tf
+++ providers.tf
@@ -7,5 +7,6 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region   = "us-east-1"
+  role_arn = "arn:aws:iam::847855290887:role/lambda-apigateway-role-Jan21-2023"
 }
